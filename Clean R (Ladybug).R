aa889c118574f1319220cd8e1f390ce836c593d0
vieta302 <118494078+vieta302@users.noreply.github.com>
2022-12-02 11:16:07 -0600

    b

---
+++ Clean R (Ladybug).R
@@ -0,0 +1,29 @@
+library(readxl)
+library(tidyverse)
+library(dplyr)
+library(lubridate)
+rm(list = ls())
+
+setwd("~/Desktop/Fall 2022/DATA 331/Final /Project-Insect-Carnivore")
+df <- read.csv("data/Scan Ladybug Data.csv")
+
+#Choosing Columns 
+df_final <- df %>%
+  dplyr::select("id", "scientificName", "stateProvince", "year")
+
+#Clean Id
+df_final %>% 
+  dplyr::distinct(id)
+
+#Clean Name
+df_final$scientificName[df$scientificName == ""] <- "Unknown"
+
+#Clean State   
+df_final$stateProvince[df$stateProvince == "IL"] <- "Illinois"
+df_final$stateProvince[df$stateProvince == "IA"] <- "Iowa"
+df_final$stateProvince[df$stateProvince == "Ia"] <- "Iowa"
+df_final$stateProvince[df$stateProvince == "Il"] <- "Illinois"
+
+#Create new file
+write.csv(df_final, "~/Desktop/Fall 2022/DATA 331/Final /Project-Insect-Carnivore/data/clean_data.csv", row.names = FALSE)
+
